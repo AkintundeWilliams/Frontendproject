065283b5b9bde7b8851bdbb6289746de6612741f
marko-a <marko_aligrudic@yahoo.com>
2022-11-08 17:55:03 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/Footer.js 	modified:   src/components/Login/Login.js 	modified:   src/components/Login/LoginForm.js 	modified:   src/components/Pages/Home.js 	modified:   src/css/Body.css 	modified:   src/css/Footer.css 	modified:   src/css/Header.css 	modified:   src/css/Home.css 	modified:   src/css/Navbar.css 	new file:   src/server.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
+import React, { useState } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Pages/Home';
 import { BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
+import styled from 'styled-components';
+import { findByLabelText } from '@testing-library/react';
 
+export const Grid = styled.div;
 
+export const Row = styled.div
+  display: flex;
+  ;
+
+export const Col = styled.div
+  display: flex;
+;
 function App() {
+  const [token, setToken] = useState();
+
+  console.log(token);
+ 
   return (
     <Router>
       <Switch>
         <Route path="/" exact>
           {<Login/>}
-        </Route> 
-
+        </Route>
         <Route path="/home" exact>
          {<Home/>} 
         </Route> 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import '../css/Footer.css'
 const Footer = () => {
     return (
       <>
-          <footer className='test1234'>@2022 DZ HYP AG</footer>
+          <footer className='test1234'>@{new Date().getFullYear()} DZ HYP AG</footer>
       </>
     )
   }
